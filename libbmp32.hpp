62b942909d5fbd08dc7987422cc6647e0157e6ea
KANI Tetsuro <doscoy.t@gmail.com>
2014-01-10 01:15:48 +0900

    classing..

--- libbmp32.hpp
+++ libbmp32.hpp
@@ -2,50 +2,86 @@
 #ifndef LIB_BMP32_HPP
 #define LIB_BMP32_HPP
 
-#define LIBBMP_VERSION  1000
-
+#define LIBBMP_VERSION      1001
+#define LIBBMP_SIGNATURE    19778
 
 #include <cstdlib>
-#include <iostream>
 #include <cstdint>
 #include <fstream>
 
 
-namespace libbmp32 {
+namespace libbmp24 {
 
 
 //------------------------------------------------------------------------------------------
 //  ビットマップファイルヘッダ
-#pragma pack(1)
-
-struct BitmapFileHeader {
-    uint16_t type_;
-    uint32_t size_;
-    uint16_t reserved1_;
-    uint16_t reserved2_;
-    uint32_t offbits_;
-};
-
-struct BitmapInfoHeader{
-  uint32_t size_;
-  int32_t width_;
-  int32_t heigth_;
-  uint16_t planes_;
-  uint16_t bit_count_;
-  uint32_t compression_;
-  uint32_t size_image_;
-  int32_t pels_per_meter_x_;
-  int32_t pels_per_meter_y_;
-  uint32_t clr_used_;
-  uint32_t clr_important_;
-};
 
 
-#pragma pack()
+class Bitmap {
 
+public:
+    struct FileHeader {
+        uint32_t size_;
+        uint16_t reserved1_;
+        uint16_t reserved2_;
+        uint32_t offbits_;
 
-struct Bitmap
-{
+        void setup(const int image_size) {
+            size_ = image_size + sizeof(FileHeader) + sizeof(InfoHeader);
+            reserved1_ = 0;
+            reserved2_ = 0;
+            offbits_ = sizeof(FileHeader) + sizeof(InfoHeader);
+        }
+    };
+
+    struct InfoHeader{
+        uint32_t size_;
+        int32_t width_;
+        int32_t heigth_;
+        uint16_t planes_;
+        uint16_t bit_count_;
+        uint32_t compression_;
+        uint32_t size_image_;
+        int32_t pels_per_meter_x_;
+        int32_t pels_per_meter_y_;
+        uint32_t clr_used_;
+        uint32_t clr_important_;
+
+        void setup(
+            const int width,
+            const int height
+        ){
+            int padding = (width * 3 + 3) / 4 * 4 - width * 3;    // 32bit境界条件によるパディング
+            int img_size = (width * 3 + padding) * height;        // 出力される場増データサイズ
+
+            size_ = sizeof(InfoHeader);
+            width_ = width;
+            heigth_ = height;
+            planes_ = 1;
+            bit_count_ = 24;
+            compression_ = 0;
+            size_image_ = img_size;
+            pels_per_meter_x_ = 0;
+            pels_per_meter_y_ = 0;
+            clr_used_ = 0;
+            clr_important_ = 0;
+        }
+
+        void dump() const {
+            printf("size = %d\n", size_);
+            printf("width = %d\n", width_);
+            printf("heigth = %d\n", heigth_);
+            printf("planes = %d\n", planes_);
+            printf("bit_count = %d\n", bit_count_);
+            printf("compression = %d\n", compression_);
+            printf("size_image = %d\n", size_image_);
+            printf("pels_per_meter_x = %d\n", pels_per_meter_x_);
+            printf("pels_per_meter_y = %d\n", pels_per_meter_y_);
+            printf("clr_used = %d\n", clr_used_);
+            printf("clr_important = %d\n", clr_important_);
+        }
+    };
+
     Bitmap()
         : file_header_()
         , info_header_()
@@ -53,7 +89,7 @@ struct Bitmap
     {}
 
     ~Bitmap() {
-        delete data_;
+        delete[] data_;
     }
 
 
@@ -69,195 +105,166 @@ struct Bitmap
         return info_header_.size_image_;
     }
 
-    BitmapFileHeader file_header_;
-    BitmapInfoHeader info_header_;
+    const uint8_t* getData() const {
+        return data_;
+    }
+
+    void dump() const {
+        info_header_.dump();
+    }
+
+    void fill(int r, int g, int b) {
+        int width = getWidth();
+        int height = getHeight();
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                int pixel_idx = x + (y * width);
+                int data_idx = pixel_idx * 3;
+
+                data_[data_idx + 0] = static_cast<uint8_t>(b);
+                data_[data_idx + 1] = static_cast<uint8_t>(g);
+                data_[data_idx + 2] = static_cast<uint8_t>(r);
+            }
+        }
+    }
+
+private:
+    FileHeader file_header_;
+    InfoHeader info_header_;
     uint8_t* data_;
+
+public:
+    //-------------------------------------------
+    //  ビットマップ生成
+    void createBitmap(
+        int width,
+        int height
+    ) {
+        info_header_.setup(width, height);
+        int image_size = getImageSize();
+        file_header_.setup(image_size);
+        delete[] data_;
+        data_ = new uint8_t[image_size];
+    }
+
+
+    //------------------------------------------------------------------------------------------
+    //  ビットマップ書き出し.
+    bool serialize(
+        std::ofstream& file
+    ) const {
+        
+        //  ----------------------------------
+        //  ヘッダ出力
+        uint16_t bm_signature = LIBBMP_SIGNATURE;
+        file.write((char*)&bm_signature, 2);
+        file.write((char*)&file_header_, sizeof(FileHeader));
+        file.write((char*)&info_header_, sizeof(InfoHeader));
+
+        //  内容出力
+        file.write((char*)data_, getImageSize());
+
+        //  成功
+        return true;
+
+    }
+
+
+    //------------------------------------------------------------------------------------------
+    /**
+     *  ビットマップの整合性チェック.
+     */
+    bool isVaildate() const {
+        if (info_header_.size_ != sizeof(InfoHeader)) {
+            return false;
+        }
+        if (info_header_.compression_ != 0) {
+            return false;
+        }
+
+        int pallet_size = file_header_.offbits_ - sizeof(FileHeader) - sizeof(InfoHeader);
+        int img_size = info_header_.size_image_;     // イメージサイズ
+        int x = info_header_.width_;                 //  イメージの幅
+        int y = info_header_.heigth_;                // イメージの高さ
+        int color_bit = info_header_.bit_count_;     // １色あたりのビット数
+
+
+        //  ヘッダの矛盾を確認
+        if (color_bit  != 24) {
+            return false;
+        }
+
+        if (color_bit  == 0) {
+            return false;
+        }
+
+        if (x <= 0 || y <= 0) {
+            return false;
+        }
+
+        if (img_size != file_header_.size_ - file_header_.offbits_) {
+            return false;
+        }
+
+        if (img_size != (x * color_bit + 31) / 32 * 4 * y) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //------------------------------------------------------------------------------------------
+    //  ビットマップ読み込み. fstream ver.
+    bool deserialize (
+        std::ifstream& file
+    ) {
+
+        //  シグネチャ確認
+        uint16_t signature = 0;
+        file.read((char*)(&signature), 2);
+
+        if (signature != LIBBMP_SIGNATURE) {
+            //  ビットマップファイルではない
+            return false;
+        }
+
+        //  ヘッダ取得
+        file.read((char *)(&file_header_), sizeof(FileHeader));
+        if( file.bad() ) {
+            //  読み込み失敗
+            return false;
+        }
+
+        //  ファイルヘッダ
+        file.read((char *)(&info_header_), sizeof(InfoHeader));
+        if (file.bad()) {
+            //  読み込み失敗
+            return false;
+        }
+
+
+        if (!isVaildate()) {
+            //  不正なデータ
+            return false;
+        }
+
+        //  イメージデータ取得
+        int img_size = getImageSize();
+        delete[] data_;
+        data_ = new uint8_t[img_size];
+
+        if (!data_) {
+            //  読み込み失敗
+            return false;
+        }
+
+        file.read((char *)data_, img_size);
+
+        return true;
+    }
 };
 
-
-
-/**
- *  ビットマップファイルヘッダ構築.
- */
-void setupBitmapFileHeader(
-    BitmapFileHeader* const file_header,
-    const int img_size
-){
-    file_header->type_ = *(uint16_t*)"BM";  // TODO 定数アクセスする
-    file_header->size_ = img_size + sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
-    file_header->reserved1_ = 0;
-    file_header->reserved2_ = 0;
-    file_header->offbits_ = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
-}
-
-/**
- *  ビットマップインフォヘッダ構築.
- */
-void setupBitmapInfoHeader(
-    BitmapInfoHeader* const info_header,
-    const int x,
-    const int y,
-    const int img_size
-){
-    info_header->size_ = sizeof(BitmapInfoHeader);
-    info_header->width_ = x;
-    info_header->heigth_ = y;
-    info_header->planes_ = 1;
-    info_header->bit_count_ = 24;
-    info_header->compression_ = 0;
-    info_header->size_image_ = img_size;
-    info_header->pels_per_meter_x_ = 0;
-    info_header->pels_per_meter_y_ = 0;
-    info_header->clr_used_ = 0;
-    info_header->clr_important_ = 0;
-}
-
-
-
-//------------------------------------------------------------------------------------------
-//  ビットマップ書き出し. fstream ver.
-bool serializeBmp(
-    std::ofstream* file, 
-    const Accessor* const accessor
-){
-    if(file == nullptr || accessor == nullptr) {
-        return false;
-    }
-
-    //  ----------------------------------
-    //  ヘッダ作成
-    int x = accessor->getWidth();
-    int y = accessor->getHeight();
-    int padding = (x * 3 + 3) / 4 * 4 - x * 3;      // 32bit境界条件によるパディング
-    int img_size = (x * 3 + padding) * y;           // 出力される場増データサイズ
-
-    //  ファイルヘッダ
-    BitmapFileHeader file_header;
-    setupBitmapFileHeader(&file_header, img_size);
-
-    //  インフォヘッダ
-    BitmapInfoHeader info_header;
-    setupBitmapInfoHeader(
-        &info_header,
-        x,
-        y,
-        img_size
-    );
-
-    //  ----------------------------------
-    //  ヘッダ出力
-    file->write((char*)&file_header, sizeof(BitmapFileHeader));
-    file->write((char*)&info_header, sizeof(BitmapInfoHeader));
-
-    //  内容出力
-    file->write((char*)accessor->data_, img_size);
-
-    //  成功
-    return true;
-
-}
-
-
-//------------------------------------------------------------------------------------------
-/**
- *  ビットマップの整合性チェック.
- */
-bool isVaildBmp(
-    const BitmapFileHeader* const file_header,
-    const BitmapInfoHeader* const info_header
-){
-    if (file_header->type_ != *(uint16_t*)"BM") { // TODO 定数判定に変える
-        return false;
-    }
-    if (info_header->size_ != sizeof(BitmapInfoHeader)) {
-        return false;
-    }
-    if (info_header->compression_ != 0) {
-        return false;
-    }
-
-    int pallet_size = file_header->offbits_ - sizeof(BitmapFileHeader) - sizeof(BitmapInfoHeader);
-    int img_size = info_header->size_image_;     // イメージサイズ
-    int x = info_header->width_;                 //  イメージの幅
-    int y = info_header->heigth_;                // イメージの高さ
-    int color_bit = info_header->bit_count_;     // １色あたりのビット数
-
-
-    //  ヘッダの矛盾を確認
-    if (color_bit  != 24) {
-        return false;
-    }
-
-    if (color_bit  == 0) {
-        return false;
-    }
-
-    if (x <= 0 || y <= 0) {
-        return false;
-    }
-
-    if (img_size != file_header->size_ - file_header->offbits_) {
-        return false;
-    }
-
-    if (img_size != (x * color_bit + 31) / 32 * 4 * y) {
-        return false;
-    }
-
-    return true;
-}
-
-
-//------------------------------------------------------------------------------------------
-//  ビットマップ読み込み. fstream ver.
-bool deserializeBmp(
-    std::ifstream* file,
-    Accessor* accessor
-) {
-
-    if(file == nullptr) {
-        return false;
-    }
-
-    //  ヘッダ取得
-    file->read((char *)(&accessor->file_header_), sizeof(BitmapFileHeader));
-    if( file->bad() ) {
-        //  読み込み失敗
-        return nullptr;
-    }
-
-    //  ファイルヘッダ
-    file->read((char *)(&accessor->info_header_), sizeof(BitmapInfoHeader));
-    if( file->bad() ) {
-        //  読み込み失敗
-        return nullptr;
-    }
-
-
-    if (!isVaildBmp(&accessor->file_header_, &accessor->info_header_)) {
-        //  不正なデータ
-        return false;
-    }
-
-    //  イメージデータ取得
-    int img_size = accessor->getImageSize();
-    uint8_t* buf = (uint8_t*)malloc(img_size);
-    uint8_t* buf_top = buf;
-
-    if (buf == nullptr) {
-        //  読み込み失敗
-        return false;
-    }
-
-
-    file->read((char *)buf, img_size);
-
-    //  ビットマップからイメージデータ生成
-    accessor->data_ = buf;
-
-    //   片付け
-    return true;
-}
 
 
 }   // namespace libbmp32
